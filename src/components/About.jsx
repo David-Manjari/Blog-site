6e2a46e2c7eae63813e81abac9920f9457308604
Manjari <manjari@Manjari.localdomain>
2026-05-26 18:59:59 +0300

    created an aside element for about file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function About (props){
     return (
-        <div>
-            <img src={props.imageUrl } alt="blog logo" width={400} height = {250} />
-            <p>{props.about}</p>
-        </div>
+            <aside>
+                <img src={props.imageUrl} alt="blog logo" width={400} height={250} />
+                <p>{props.about}</p>
+            </aside>
     )
 }
 
